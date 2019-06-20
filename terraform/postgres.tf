c070302241726f21db7cd90004275d45377afcbe
andrii Perun <perandr@gmail.com>
2019-06-21 00:24:13 +0300

    Added instance.tf with simple instance config. Commented ot code to provision postgres.

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -1,22 +1,22 @@
-resource "google_sql_database_instance" "postgres" {
-  name = "postgres"
-  # count = "${var.usePG ? 1 : 0}"
-  database_version = "POSTGRES_9_6"
-  region       = "${var.region}"
+# resource "google_sql_database_instance" "postgres" {
+#   name = "postgres"
+#   # count = "${var.usePG ? 1 : 0}"
+#   database_version = "POSTGRES_9_6"
+#   region       = "${var.region}"
 
-  settings {
-    tier = "db-f1-micro"
-  }
-}
+#   settings {
+#     tier = "db-f1-micro"
+#   }
+# }
 
-resource "google_sql_database" "database" {
-  name      = "udb"
-  instance  = "${google_sql_database_instance.postgres.name}"
-}
+# resource "google_sql_database" "database" {
+#   name      = "udb"
+#   instance  = "${google_sql_database_instance.postgres.name}"
+# }
 
-resource "google_sql_user" "users" {
-  name     = "postgres"
-  instance = "${google_sql_database_instance.postgres.name}"
-  host     = "*"
-  password = "${var.db_user_pwd}"
-}
+# resource "google_sql_user" "users" {
+#   name     = "postgres"
+#   instance = "${google_sql_database_instance.postgres.name}"
+#   host     = "*"
+#   password = "${var.db_user_pwd}"
+# }
